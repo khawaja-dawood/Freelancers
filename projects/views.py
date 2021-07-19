21177ea731d2893bda3011ac49332d5790d993c7
Khawaja Dawood <dawoodnaveed0@gmail.com>
2021-07-19 20:02:56 +0500

    added table in project page

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website',
+        'topRated': True
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work',
+        'topRated': False
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community',
+        'topRated': True
+    }
+]
+
 
 def projects(request):
-    return render(request=request, template_name='projects/projects.html')
+
+    context = {'projects': projectsList}
+    return render(request=request, template_name='projects/projects.html', context=context)
 
 
 def project(request, pk):
-    return render(request=request, template_name='projects/single-project.html')
+    projectObject = None
+    for i in projectsList:
+        if i['id'] == str(pk):
+            projectObject = i
+    context = {'project': projectObject}
+    return render(request=request, template_name='projects/single-project.html', context=context)
