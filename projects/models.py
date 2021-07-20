5f07b1c1623effeca1c69ecb2dadec14741b5420
Khawaja Dawood <dawoodnaveed0@gmail.com>
2021-07-20 22:15:02 +0500

    fixed a bug

--- projects/models.py
+++ projects/models.py
@@ -33,7 +33,7 @@ class Review(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return self.value
+        return self.value + " - " + str(self.project)
 
 
 class Tag(models.Model):
